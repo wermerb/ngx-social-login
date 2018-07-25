600fd6b70c4a3d6ec47eb9a45b1322317f44e19c
Balázs Wermer <wermerb@gmail.com>
2018-07-25 13:08:04 +0200

    Fix demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,21 @@ import { AppComponent } from './app.component';
 import { NgxSocialLoginModule } from 'ngx-social-login';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    NgxSocialLoginModule.init({
-      google: {
-        client_id: '994632666969-qdss3kuen6iicsm2pe2ooecvpufnrevj.apps.googleusercontent.com'
-      },
-      facebook: {
-        initOptions: {
-          appId: '1490265507765668'
-        }
-      }
-    })
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        NgxSocialLoginModule.init({
+            google: {
+                client_id: 'YOUR_CLIENT_ID'
+            },
+            facebook: {
+                initOptions: {
+                    appId: 'YOUR_APP_ID'
+                }
+            }
+        })
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
